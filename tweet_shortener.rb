f1f2fb16e7d5497eb65017aea0ae8fe06c645d56
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-17 22:02:35 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+require 'pry'
+def dictionary
+  dictionary = {"hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(string)
+  string.split.collect do |element|
+    if dictionary.keys.include?(element)
+      element = dictionary[element]
+    end 
+  end.join(" ")
+end 
